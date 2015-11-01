225ca87cacc6e97e2b614e16d6b09a4953443a57
Pascal Garber <pascal@jumplink.eu>
2015-11-01 22:26:02 +0100

    bugfix

--- assets/js/controllers/DocsTutorialController.js
+++ assets/js/controllers/DocsTutorialController.js
@@ -1,10 +1,5 @@
 docs.controller('DocsTutorialController', function($scope, tutorial, $state, $log, HistoryService) {
   var page = $state.current.name;
-
   $scope.tutorial = tutorial;
-  $log.debug($scope.tutorial);
-
-  // $log.debug("[DocsOverviewController:docs]", $scope.docs);
   $scope.goTo = HistoryService.goToHashPosition;
-
 });
